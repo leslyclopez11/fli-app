7a7c9f74e92e2d2ff95f93bfa178706f8ae0a233
clarabkwon <clarabkwon@gmail.com>
2024-08-20 15:09:28 -0400

    look over pictures in about page

--- fli-app/landingPage.js
+++ fli-app/landingPage.js
@@ -2,3 +2,13 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
     const navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('active');
   });
+
+  document.getElementById('show-mailchimp-form').addEventListener('click', function() {
+    var form = document.getElementById('mc_embed_shell');
+    if (form.style.display === 'none' || form.style.display === '') {
+      form.style.display = 'block';
+    } else {
+      form.style.display = 'none';
+    }
+  });
+  
